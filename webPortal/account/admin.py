fde7001bed3b9dce4bbedd96ed6b1eae5cbf222e
Rushi Patel <developerr@Rushis-MacBook-Air.local>
2022-08-08 01:39:45 +0530

    :hammer: Profile model created

--- webPortal/account/admin.py
+++ webPortal/account/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
-
+from account.models import Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'Profiles'
+
+class CustomizedUserAdmin (UserAdmin):
+    inlines = (ProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User,CustomizedUserAdmin)
+admin.site.register(Profile)
--- webPortal/account/models.py
+++ webPortal/account/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    faculty_name = models.CharField(max_length=70)
+    designation = models.CharField(max_length=200)
+    employee_id = models.PositiveBigIntegerField()
+    email = models.EmailField()
+    contact_number = models.PositiveBigIntegerField()
+    date_of_joining = models.DateField(auto_created=True)
+
+
+    def __str__(self):
+        return self.faculty_name
